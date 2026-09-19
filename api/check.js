6591e2eb96a932d0da8c10bf55a898055fa4859b
y4cs <y5744854@gmail.com>
2026-09-19 18:04:06 +0300

    Change 'TEST-KEY-123' status to active
    Updated the status of 'TEST-KEY-123' from banned to active.

--- api/check.js
+++ api/check.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
     // قائمة المفاتيح: يمكنك إضافة وتعديل المفاتيح من هنا مباشرة
     const keys = [
         { key: "VIP-2026-ABC", expires: "2026-12-31", banned: false },
-        { key: "TEST-KEY-123", expires: "2025-01-01", banned: true }
+        { key: "TEST-KEY-123", expires: "2025-01-01", banned: false }
     ];
 
     const foundKey = keys.find(k => k.key === key);
